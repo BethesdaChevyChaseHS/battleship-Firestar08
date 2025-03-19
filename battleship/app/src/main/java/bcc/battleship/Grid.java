3643acd625d0a9586d8a9630c886521b94b7cbee
Firestar08 <148727676+Firestar08@users.noreply.github.com>
2025-03-19 17:37:02 +0000

    submitting

--- battleship/app/src/main/java/bcc/battleship/Grid.java
+++ battleship/app/src/main/java/bcc/battleship/Grid.java
@@ -9,6 +9,11 @@ public class Grid {
 
     public Grid() {
         g = new Location[Constants.NUM_ROWS][Constants.NUM_COLS];
+        for (int row = 0; row < Constants.NUM_ROWS; row++){
+            for (int col = 0; col < Constants.NUM_COLS; col++){
+                g[row][col] = new Location();
+            }
+        }
     }
     
     // Mark a hit in the specified location
@@ -65,21 +70,21 @@ public class Grid {
     //maybe convert to boolean?
     public boolean addShip(Ship s) {
         if (s.getDirection() == Constants.HORIZONTAL){
-            if (s.getCol() + s.getLength() >= Constants.NUM_ROWS){
+            if (s.getCol() + s.getLength() >= this.numCols()){
                 return false;
             }
-            for (int i = s.getCol(); i < s.getCol() + s.getLength(); i++){
-                if (g[s.getRow()][i].hasShip()){
+            for (int col = s.getCol(); col < s.getCol() + s.getLength(); col++){
+                if (g[s.getRow()][col].hasShip() == true){
                     return false;
                 } 
             }
         }
         else {
-            if (s.getRow() + s.getLength() >= Constants.NUM_COLS){
+            if (s.getRow() + s.getLength() >= this.numRows()){
                 return false;
             }
-            for (int i = s.getRow(); i < s.getRow() + s.getLength(); i++){
-                if (g[i][s.getRow()].hasShip()){
+            for (int row = s.getRow(); row < s.getRow() + s.getLength(); row++){
+                if (g[row][s.getCol()].hasShip() == true){
                     return false;
                 }
             }
@@ -88,9 +93,9 @@ public class Grid {
     }
 
     public boolean allShipsSank(){
-        for (int i = 0; i < Constants.NUM_COLS; i++){
-            for (int j = 0; j < Constants.NUM_ROWS; j++){
-                if (g[i][j].hasShip() && (g[i][j].getStatus() == Constants.UNGUESSED)){
+        for (int row = 0; row < Constants.NUM_ROWS; row++){
+            for (int col = 0; col < Constants.NUM_COLS; col++){
+                if (g[row][col].hasShip() == true && g[row][col].getStatus() == Constants.UNGUESSED){
                     return false;
                 }
             }
